c334b0ba1cfbc7282d5d74d23623fa53663eadb1
julieritz <julie.ritz@gmail.com>
2020-08-28 10:10:41 -0400

    Updates

---
+++ precache-manifest.bcfbe067b692617a08e3eb41b14d42b6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b46646f4b43c16ec237d54ae0c9c1f3",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "c1142d5740e0e9817507",
+    "url": "/React-Portfolio/static/css/main.d0366851.chunk.css"
+  },
+  {
+    "revision": "b874db580fd0fe08f2cb",
+    "url": "/React-Portfolio/static/js/2.6aa5c9f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.6aa5c9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1142d5740e0e9817507",
+    "url": "/React-Portfolio/static/js/main.e1f7d68e.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "9bb831c4f4e62f58030cfd909dce077e",
+    "url": "/React-Portfolio/static/media/Budget-Tracker.9bb831c4.png"
+  },
+  {
+    "revision": "830a845ab9e2040da906aab71405609c",
+    "url": "/React-Portfolio/static/media/Code-Quiz.830a845a.png"
+  },
+  {
+    "revision": "2adaa64cdf273f475a0b8d66517053af",
+    "url": "/React-Portfolio/static/media/Date-Night-Helper.2adaa64c.png"
+  },
+  {
+    "revision": "707c454f988a320573ac8465bc866555",
+    "url": "/React-Portfolio/static/media/Eat-Da-Burger.707c454f.png"
+  },
+  {
+    "revision": "dda419fa178a3f0c03184f665a19bf66",
+    "url": "/React-Portfolio/static/media/Employee-Directory.dda419fa.png"
+  },
+  {
+    "revision": "2444ee1a12af3a9c027f176b44be5fcb",
+    "url": "/React-Portfolio/static/media/Employee-Tracker.2444ee1a.png"
+  },
+  {
+    "revision": "7a68e5499d46b7309595fc7a9df2570b",
+    "url": "/React-Portfolio/static/media/Fitness-Tracker.7a68e549.png"
+  },
+  {
+    "revision": "858fb26cfc7356161a02f4184b505015",
+    "url": "/React-Portfolio/static/media/Note-Taker.858fb26c.png"
+  },
+  {
+    "revision": "4effbd72645e48a72619d0be55482f52",
+    "url": "/React-Portfolio/static/media/Represent-Me.4effbd72.png"
+  },
+  {
+    "revision": "51f91e81cce597ad29f69d65de1045bd",
+    "url": "/React-Portfolio/static/media/Template-Engine.51f91e81.png"
+  },
+  {
+    "revision": "b468a2dc21f38aa6efab7bbae16c4469",
+    "url": "/React-Portfolio/static/media/Tiny-Learn.b468a2dc.png"
+  },
+  {
+    "revision": "8e14c63931dc07f65b36248f1dfb3084",
+    "url": "/React-Portfolio/static/media/Work-Day-Scheduler.8e14c639.png"
+  },
+  {
+    "revision": "daf3740893b4a25062d41e9ae6e3ad3a",
+    "url": "/React-Portfolio/static/media/circle-cropped.daf37408.png"
+  },
+  {
+    "revision": "058035fa42f9aa78759ac1bd68092c5b",
+    "url": "/React-Portfolio/static/media/flowers-left.058035fa.jpg"
+  },
+  {
+    "revision": "4618a560dede735553d6b1edf2bd6936",
+    "url": "/React-Portfolio/static/media/flowers-right.4618a560.jpg"
+  },
+  {
+    "revision": "c9dfc8ed50e7887018b0c3cef4d37144",
+    "url": "/React-Portfolio/static/media/long-flowers.c9dfc8ed.jpg"
+  }
+]);
